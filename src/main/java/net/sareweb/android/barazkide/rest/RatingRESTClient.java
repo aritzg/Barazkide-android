89a83e2ca77b25ef3a587771879fa714f496f6fa
Aritz Galdos <galdos.aritz@gmail.com>
2012-12-17 21:55:29 +0100

    Added  addRating rest calñ

--- src/main/java/net/sareweb/android/barazkide/rest/RatingRESTClient.java
+++ src/main/java/net/sareweb/android/barazkide/rest/RatingRESTClient.java
@@ -1,7 +1,12 @@
 package net.sareweb.android.barazkide.rest;
 
+import net.sareweb.android.barazkide.model.Garden;
 import net.sareweb.android.barazkide.model.Rating;
 import net.sareweb.lifedroid.rest.generic.LDRESTClient;
+
+import org.springframework.http.HttpMethod;
+
+import android.util.Log;
 
 public class RatingRESTClient extends LDRESTClient<Rating> {
 
@@ -9,6 +14,21 @@ public class RatingRESTClient extends LDRESTClient<Rating> {
 		super(connectionData);
 	}
 	
+	public Rating addRating(long userId, String rateType, long ratedObjectId, double rate){
+		String requestURL = getBaseURL() + "/add-rating";
+		requestURL = addParamToRequestURL(requestURL, "user-id", userId);
+		requestURL = addParamToRequestURL(requestURL, "rate-type", rateType);
+		requestURL = addParamToRequestURL(requestURL, "rated-object-id", ratedObjectId);
+		requestURL = addParamToRequestURL(requestURL, "rate", rate);
+		return run(requestURL, HttpMethod.POST);
+	}
+	
+	public double getAvgRating(long ratedObjectId){
+		String requestURL = getBaseURL() + "/get-avg-rating";
+		requestURL = addParamToRequestURL(requestURL, "rated-object-id", ratedObjectId);
+		Log.d(TAG, "requestURL " + requestURL);
+		return runForDouble(requestURL, HttpMethod.GET);
+	}
 
 	@Override
 	public String getPorltetContext() {
